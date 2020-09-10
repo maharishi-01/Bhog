3776dd698d61db29d3fa835e0ae71b7fee4e940c
Rishi <rishiraj619march@gmail.com>
2020-09-10 23:20:32 +0530

    Added Splash Screen page

--- app/src/main/java/com/example/bhog/introductoryActivity.java
+++ app/src/main/java/com/example/bhog/introductoryActivity.java
@@ -3,12 +3,30 @@ package com.example.bhog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class introductoryActivity extends AppCompatActivity {
 
+    ImageView logo,splashImage;
+    TextView appmame;
+    LottieAnimationView lottieAnimationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introductory);
+
+        logo = findViewById(R.id.logo);
+        appmame = findViewById(R.id.app_name);
+        splashImage = findViewById(R.id.image);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+
+        splashImage.animate().translationY(-2300).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+        appmame.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
     }
 }
